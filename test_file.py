cc53ee4ec2ffa8e5a8f7d3f5ffacac4a86bdd735
RahulBoyanapalli <113788939+RahulBoyanapalli@users.noreply.github.com>
2022-12-17 21:08:55 +0530

    added Marks_to_per

--- test_file.py
+++ test_file.py
@@ -5,4 +5,13 @@ a = 1
 b = 2
 answer = sum([a, b])
 print(answer)
-print("Hello")
+
+
+def marks_to_per():
+    marks = input("enter markes secured : ")
+    max_marks = input("maximum mark in subject : ")
+    per = (int(marks) / int(max_marks)) * 100
+    print(per)
+
+
+marks_to_per()
